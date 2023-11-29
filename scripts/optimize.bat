a24071d410ad02284dec13446aa80cf54149d70c
Gabriel Lopez <glopez11193@gmail.com>
2023-11-29 03:04:26 -0600

    Use cosmwasm optimizer v15

--- scripts/optimize.bat
+++ scripts/optimize.bat
@@ -1,1 +1,1 @@
-docker run --rm -v "%cd%":/code --mount type=volume,source=arena_cache,target=/code/target --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry cosmwasm/workspace-optimizer:0.14.0
+docker run --rm -v "%cd%":/code --mount type=volume,source=arena_cache,target=/code/target --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry cosmwasm/optimizer:0.15.0
