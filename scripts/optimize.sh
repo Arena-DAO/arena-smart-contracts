f578be26c296c0f69b61dda50c223a4462e63047
Gabriel Lopez <glopez11193@gmail.com>
2024-03-07 10:40:09 -0600

    Create optimize.sh

---
+++ scripts/optimize.sh
@@ -0,0 +1,5 @@
+docker run --rm \
+  -v "$(pwd)":/code \
+  --mount type=volume,source=arena_cache,target=/code/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/optimizer:0.15.0
