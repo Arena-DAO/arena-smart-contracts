70c684b451edf0942e8aeb5883edb438c00c11f3
ismellike <glopez11193@gmail.com>
2024-12-13 00:25:36 -0600

    Schema and deploy identity

---
+++ contracts/arena-discord-identity/src/bin/discord.rs
@@ -0,0 +1,11 @@
+use arena_discord_identity::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use cosmwasm_schema::write_api;
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+        migrate: MigrateMsg,
+    }
+}
