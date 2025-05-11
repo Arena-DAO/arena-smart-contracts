a9ed3b948ae644af121163a338d50b1c26df07ed
ismellike <glopez11193@gmail.com>
2025-05-10 21:05:58 -0500

    Do not use unstable is_none_or

--- contracts/arena-escrow/src/state.rs
+++ contracts/arena-escrow/src/state.rs
@@ -74,7 +74,8 @@ pub fn paginate_balances_for(
     let mut seen = BTreeSet::new();
     for addr in iter {
         let addr = addr?;
-        if start.as_ref().is_none_or(|s| addr > *s) {
+        #[allow(clippy::unnecessary_map_or)]
+        if start.as_ref().map_or(true, |s| addr > *s) {
             seen.insert(addr);
             if seen.len() == limit {
                 break;
